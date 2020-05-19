af483fed15be543c2779a2bc4a9fa71d31106d66
Foo Chi Siong <chisiong.foo@softspace.com.my>
2020-05-19 16:21:12 +0800

    Added AddBook function (.kt & layout)

---
+++ app/src/main/java/com/example/bookstore/AddBook.kt
@@ -0,0 +1,12 @@
+package com.example.bookstore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddBook : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_book)
+    }
+}
