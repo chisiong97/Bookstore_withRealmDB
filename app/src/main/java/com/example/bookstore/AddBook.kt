b3d25217ca6b1f9a57e0c6a21e8e96ed56f06704
Foo Chi Siong <chisiong.foo@softspace.com.my>
2020-05-27 16:12:15 +0800

    AddBook.kt : Added camera runtime permission

--- app/src/main/java/com/example/bookstore/AddBook.kt
+++ app/src/main/java/com/example/bookstore/AddBook.kt
@@ -1,17 +1,32 @@
 package com.example.bookstore
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
-import android.view.Menu
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.PopupMenu
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_add_book.*
+import kotlinx.android.synthetic.main.activity_add_book.author
+import kotlinx.android.synthetic.main.activity_add_book.book_title
+import kotlinx.android.synthetic.main.activity_add_book.description
+import kotlinx.android.synthetic.main.activity_add_book.imageView
+import kotlinx.android.synthetic.main.activity_book_details.*
 import kotlinx.android.synthetic.main.addbook_action_bar_layout.*
+import java.security.AccessController.getContext
 
 class AddBook : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_book)
@@ -27,6 +42,7 @@ class AddBook : AppCompatActivity() {
             var bookDesc = description.text.toString()
 
             //TODO: Get image loaded by user
+            //TODO: Load image from local, Save image from local,camera
 
             var newBook = Book(
                     author = bookAuthor,
@@ -44,17 +60,37 @@ class AddBook : AppCompatActivity() {
             finish()
         }
 
+
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) -> {
+                // You can use the API that requires the permission.
+            }
+            else -> {
+                //makeRequest()
+                requestPermissions(arrayOf(Manifest.permission.CAMERA),REQUEST_IMAGE_CAPTURE )
+            }
+        }
+
+
         //Show image options
         imageView.setOnClickListener(){
             println("imgBtn clicked!")
             val popupMenu: PopupMenu = PopupMenu(this, imageView)
-            popupMenu.menuInflater.inflate(R.menu.imagenav,popupMenu.menu)
+            popupMenu.menuInflater.inflate(R.menu.imageoptions,popupMenu.menu)
             popupMenu.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item ->
+                //switch case
                 when(item.itemId) {
-                    R.id.navigation_library->
+                    R.id.navigation_library->{
                         Toast.makeText(this, "You Clicked : " + item.title, Toast.LENGTH_SHORT).show()
-                    R.id.navigation_camera ->
+                        pickImageFromGallery()
+                    }
+                    R.id.navigation_camera ->{
                         Toast.makeText(this, "You Clicked : " + item.title, Toast.LENGTH_SHORT).show()
+                        dispatchTakePictureIntent()
+                    }
                     R.id.navigation_cancel ->
                         Toast.makeText(this, "You Clicked : " + item.title, Toast.LENGTH_SHORT).show()
                 }
@@ -72,5 +108,80 @@ class AddBook : AppCompatActivity() {
             finish()
         }
     }
+    val TAG = "Test camera"
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_IMAGE_CAPTURE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Log.i(TAG, "Permission has been denied by user")
+                } else {
+                    Log.i(TAG, "Permission has been granted by user")
+                }
+            }
+        }
+    }
+
+    //function for pick image from gallery
+    private fun pickImageFromGallery() {
+        //Intent to pick image
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+    companion object {
+        //image pick code
+        private val IMAGE_PICK_CODE = 1000
+        //Permission code
+        private val PERMISSION_CODE = 1001
+        //camera code
+        private val REQUEST_IMAGE_CAPTURE = 1
+    }
+
+    /*
+    //handle requested permission result
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if (grantResults.size >0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED){
+                    //permission from popup granted
+                    pickImageFromGallery()
+                }
+                else{
+                    //permission from popup denied
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+     */
+
+
+    //handle result of picked image
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK){
+            when(requestCode){
+                IMAGE_PICK_CODE ->  imageView.setImageURI(data?.data)
+                REQUEST_IMAGE_CAPTURE -> {
+                    val imageBitmap = data?.extras?.get("data") as Bitmap
+                    imageView.setImageBitmap(imageBitmap)
+                }
+            }
+        }
+    }
+
+    //function to take picture from camera
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
 
 }
